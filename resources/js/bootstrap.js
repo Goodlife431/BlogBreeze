926aeb8d01cfdee679584df61f9956070224d8e2
goodlife431 <oduwolejohn431@gmail.com>
2023-09-14 17:33:51 +0100

    problems

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,2 @@
 import { createApp, h } from 'vue';
-import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
-import { InertiaProgress } from '@inertiajs/progress';
-import 'bootstrap/dist/css/bootstrap.css';
+
--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,8 +1,10 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css')
    .postCss('resources/css/app.css', 'public/css', [
       require('tailwindcss'),
+
    ])
    .babelConfig({
       plugins: ['@babel/plugin-syntax-dynamic-import'],
